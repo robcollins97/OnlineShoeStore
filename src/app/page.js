c9b4119a48b8f97ee9779f1d3f99fba08b3ae5a0
kylegoslin@gmail.com <kylegoslin@gmail.com>
2023-10-18 13:29:36 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,168 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
-export default function Home() {
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import {ThemeProvider } from '@mui/material/styles';
+
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+
+export default function Page() {
+
+
+
+  /*
+  This function does the actual work
+  calling the fetch to get things from the database.
+  */ 
+  async function runDBCallAsync(url) {
+
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+ 
+    if(data.data== "valid"){
+      console.log("login is valid!")
+
+      
+    } else {
+
+      console.log("not valid  ")
+    }
+  }
+
+
+  /*
+
+  When the button is clicked, this is the event that is fired.
+  The first thing we need to do is prevent the default refresh of the page.
+  */
+	const handleSubmit = (event) => {
+		
+		console.log("handling submit");
+
+
+    event.preventDefault();
+  
+		const data = new FormData(event.currentTarget);
+
+
+    let email = data.get('email')
+		let pass = data.get('pass')
+
+    console.log("Sent email:" + email)
+    console.log("Sent pass:" + pass)
+
+
+    runDBCallAsync(`http://localhost:3000/api/login?email=${email}&pass=${pass}`)
+
+
+
+
+  }; // end handler
+
+
+
+
+  
+  const theme = createTheme({
+    palette: {
+     
+      secondary: {
+        main: green[500],
+      },
+    },
+  });
+  
+
+
+
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+    <Container component="main"  maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="pass"
+            label="Pass"
+            type="pass"
+            id="pass"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            Sign In
+          </Button>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </Container>
+
+    </ThemeProvider>
+
+  );
 }
